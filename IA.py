31b267d8b6f3967e55edaefff0cd3d76b8d29a1c
martin <100472279@alumnos.uc3m.es>
2023-04-30 11:08:54 +0200

    Inicio Practica Final IA

---
+++ IA.py
@@ -0,0 +1,14 @@
+
+def control_termostato():
+    max = 25
+    min = 16
+    print(max)
+    medio_grado = 0.5
+    calefaccion = False
+    calefaccion_tiempo = 1800
+    # 30 min en segundos
+
+    #if calefaccion == True:
+
+if __name__ == "__main__":
+    control_termostato()
